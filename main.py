89755b2ee9277af4f13a8501c0e147af21d4c8a8
nathanlindley <nfl6fh@virginia.edu>
2022-12-15 16:07:54 -0500

    demo mode working with questions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import pymongo
 import pandas as pd
+
+# BEGIN: From in-class demo
 import nltk
 nltk.download('punkt')
 from nltk import word_tokenize,sent_tokenize
@@ -14,6 +16,92 @@ import random
 import json
 import pickle
 
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle","rb") as f:
+        words, labels, training, output = pickle.load(f)
+
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+            
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+    labels = sorted(labels)
+
+    training = []
+    output = []
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+               bag.append(1)
+            else:
+              bag.append(0)
+    
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        
+        training.append(bag)
+        output.append(output_row)
+
+    training = np.array(training)
+    output = np.array(output)
+    
+    with open("data.pickle","wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
+
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    
+    return np.array(bag)
+
+# END: From in-class demo
+
 def main(args):
     data_ingest()
     if len(args) == 1:
@@ -24,11 +112,13 @@ def main(args):
         print('Usage: python main.py [--demo]')
 
 def demo():
+    print()
     print('Demo mode. This will not connect to Discord or OpenAI.')
     print('Enter a message to send to the bot. Enter "exit" to quit.')
     while True:
         message = input('Message: ')
         if message == 'exit':
+            print('Goodbye!')
             break
         elif message.startswith('!help'):
             print("I am a discord bot primarily designed to answer questions regarding a netflix dataset"
@@ -42,7 +132,7 @@ def demo():
         elif message.startswith('!'):
             print('Unknown command. Try !help.')
         else:
-            print('Message sent')
+            print(f'Response: {respond(message)}')
 
 def data_ingest():
     print('Ingesting data...')
@@ -145,13 +235,22 @@ def full_functionality():
         print(f'{member.name} has joined the server')
 
     # Start the Discord client
+    print('Starting Discord client...')
     client.run(os.getenv('DISCORD_TOKEN'))
 
-def read_query(query):
-    query = query.lower()
-    query = query.replace('?', '')
-    
-    return query
+def respond(query):
+    result = model.predict([bag_of_words(query, words)])[0]
+    result_index = np.argmax(result)
+    tag = labels[result_index]
+
+    if result[result_index] > 0.7:
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+        return (random.choice(responses))
+
+    else:
+        return ("I didnt get that. Can you explain or try again.")
 
 if __name__ == '__main__':
     main(sys.argv)
